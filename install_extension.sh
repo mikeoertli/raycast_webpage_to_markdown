0efb40d7f105c8d6ad0d39e42aea0377620e8256
Mike Oertli <mikeoertli@gmail.com>
2026-08-26 12:56:23 -0600

    install npm deps if needed

--- install_extension.sh
+++ install_extension.sh
@@ -6,6 +6,11 @@ NAME="Webpage to Markdown"
 STARTING_DIR="$(pwd)"
 cd "$EXTENSION_DIR"
 
+if [[ ! -x node_modules/.bin/ray ]]; then
+  echo >&2 "Installing dependencies for $NAME..."
+  npm ci || exit 1
+fi
+
 if npm run lint && npm run build ; then
   echo >&2 "✅ Installed the $NAME Raycast extension!"
 else
